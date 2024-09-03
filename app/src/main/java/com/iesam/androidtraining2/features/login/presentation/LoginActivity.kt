b6898e5a7382d5ef38bc0d4ffc88ad1bd58b44aa
Chema <jmperezra@gmail.com>
2024-09-03 14:07:51 +0200

    Ejercicio 2. Vista Login.

---
+++ app/src/main/java/com/iesam/androidtraining2/features/login/presentation/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.iesam.androidtraining2.features.login.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.iesam.androidtraining2.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+    }
+}
